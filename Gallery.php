6e98f11e56553cbe9e7db87b7b9b19b60763de13
MahloboBrian <44670747+Mahlobo-Brian@users.noreply.github.com>
2024-07-10 01:50:08 -1200

    Add files via upload

---
+++ Gallery.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="assets/img/Icon.png">	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Wo Without Men | Gallery</title>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <link href="bootstrap3/css/bootstrap.css" rel="stylesheet" />
+	<link href="assets/css/gsdk.css" rel="stylesheet" />  
+    <link href="assets/css/demo.css" rel="stylesheet" /> 
+    <!-- Font Awesome -->
+    <link href="bootstrap3/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+    <link href="assets/css/gaia.css" rel="stylesheet" type="text/css">
+	<!-- Stylesheet For Email and What-App Icon -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="assets/css/socialMediaStyle.css" rel="stylesheet"> 
+	<!-- Instafeed -->
+	<link rel='stylesheet' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
+	<link rel="stylesheet" href="Instafeed/galleryStyle.css">	
+</head>
+<body>
+
+	<!-- Nav -->
+	<div id="navbar-gallery">
+		<div class="container">
+			<nav class="navbar navbar-ct-blue navbar-transparent navbar-fixed-top" role="navigation">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Nav bar -->
+						<a href="index.php"><div class="logo-container">
+							<div class="logo">
+								<img src="assets/img/Wo-Without-Men-Logo(No-Backround).png">
+							</div></div>
+						</a>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="menu-active"><a href="index.php#intro">Home</a></li>
+							
+							<li class="dropdown">
+								<a href="" class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="index.php#about">About Us</a></li>
+									<li><a href="index.php#vision">Vision/Mission/Goals</a></li>
+									<li><a href="index.php#Objectives">Objectives</a></li>
+									<li><a href="index.php#team">Team</a></li>
+								</ul>
+							</li>
+							
+							<li><a href="#gal-h3">Gallery</a></li>
+							<li><a href="Events.php">Events</a></li>
+							<li><a href="index.php#contact">Contact Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</div> 
+	<div id="intro"></div>
+	<h3 id="gal-h3"><center/>Gallery</h3>
+	<div class="separator separator-danger">✻</div>
+	
+	<!-- -->
+	<div class="container">
+		<center/>
+		<div id="instafeed-gallery-feed" class="row no-gutter">
+			<!-- instagram feed loads here -->
+		</div>
+		<div class="post-modal-container"></div>
+		<div class="post-modal-backdrop"></div>
+	</div>
+	<!-- Socials -->
+	<div class="container" id="Gallery-Socials">
+		<div class="col-md-12">
+			<div class="row">
+				<center/>
+				<div class="col-md-12">
+					<div>
+						<p>We're on social networks</p>
+						<ul class="company-socials">   
+							<a href="https://twitter.com/wowithoutmen" id="company-socials" target="_blank" title="Follow Us On Twitter">
+								<i class="fa fa-twitter-square" style="color:#FF69B4" ></i>
+							</a>
+							
+							<a href="https://www.instagram.com/wo_without_men/" id="company-socials" target="_blank" title="Follow Us On Instagram">
+								<i class="fa fa-instagram" style="color:#FF69B4" ></i>
+							</a>
+							
+							<a href="https://www.facebook.com/Wo-Without-Men-959450460890992/" id="company-socials" target="_blank" title="Like Our Page On Facebook">
+								<i class="fa fa-facebook-square" style="color:#FF69B4" ></i>
+							</a>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer id="footer">
+		<div class="container"><br/>
+			<center/>
+			<div class="copyright">
+				&copy; Copyright. All Rights Reserved
+			</div>
+		</div>
+	</footer>
+	<!-- Back to the top button -->
+	<a href="#intro" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+	
+</body>
+
+    <script src="jquery/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+
+	<script src="bootstrap3/js/bootstrap.js" type="text/javascript"></script>
+	<script src="assets/js/gsdk-checkbox.js"></script>
+	<script src="assets/js/gsdk-radio.js"></script>
+	<script src="assets/js/gsdk-bootstrapswitch.js"></script>
+	<script src="assets/js/get-shit-done.js"></script>
+    <script src="assets/js/custom.js"></script>
+	<!-- instagram feed -->
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js'></script>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js'></script>
+	<script src="Instafeed/index.js"></script>
+	
+</html>
